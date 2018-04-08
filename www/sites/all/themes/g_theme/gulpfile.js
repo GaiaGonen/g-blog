966fda3f53f770988a6f796c463ab75d3e5fe03e
GG <gaiagonen@gmail.com>
2018-04-08 18:17:57 +0300

    Removing zen scss & styleguide, and base theme

--- www/sites/all/themes/g_theme/gulpfile.js
+++ www/sites/all/themes/g_theme/gulpfile.js
@@ -16,7 +16,6 @@ var options = {};
 // accordingly.
 options.rootPath = {
   project     : __dirname + '/',
-  styleGuide  : __dirname + '/styleguide/',
   theme       : __dirname + '/'
 };
 
@@ -54,30 +53,6 @@ options.autoprefixer = {
   ]
 };
 
-// Define the style guide paths and options.
-options.styleGuide = {
-  source: [
-    options.theme.sass,
-    options.theme.css + 'style-guide/'
-  ],
-  destination: options.rootPath.styleGuide,
-
-  builder: 'builder/twig',
-
-  // The css and js paths are URLs, like '/misc/jquery.js'.
-  // The following paths are relative to the generated style guide.
-  css: [
-    path.relative(options.rootPath.styleGuide, options.theme.css + 'styles.css'),
-    path.relative(options.rootPath.styleGuide, options.theme.css + 'style-guide/chroma-kss-styles.css'),
-    path.relative(options.rootPath.styleGuide, options.theme.css + 'style-guide/kss-only.css')
-  ],
-  js: [
-  ],
-
-  homepage: 'homepage.md',
-  title: 'Zen 7.x-6.x Style Guide'
-};
-
 // Define the paths to the JS files to lint.
 options.eslint = {
   files  : [
@@ -109,7 +84,7 @@ gulp.task('default', ['build']);
 // #################
 // Build everything.
 // #################
-gulp.task('build', ['styles:production', 'styleguide', 'lint']);
+gulp.task('build', ['styles:production', 'lint']);
 
 // ##########
 // Build CSS.
@@ -139,28 +114,6 @@ gulp.task('styles:production', ['clean:css'], function() {
     .pipe($.autoprefixer(options.autoprefixer))
     .pipe($.size({showFiles: true}))
     .pipe(gulp.dest(options.theme.css));
-});
-
-// ##################
-// Build style guide.
-// ##################
-gulp.task('styleguide', ['clean:styleguide', 'styleguide:kss-example-chroma'], function() {
-  return kss(options.styleGuide);
-});
-
-gulp.task('styleguide:kss-example-chroma', function() {
-  return gulp.src(options.theme.sass + 'style-guide/kss-example-chroma.scss')
-    .pipe(sass(options.sass).on('error', sass.logError))
-    .pipe($.replace(/(\/\*|\*\/)\n/g, ''))
-    .pipe($.rename('kss-example-chroma.twig'))
-    .pipe($.size({showFiles: true}))
-    .pipe(gulp.dest(options.theme.css + 'style-guide'));
-});
-
-// Debug the generation of the style guide with the --verbose flag.
-gulp.task('styleguide:debug', ['clean:styleguide', 'styleguide:kss-example-chroma'], function() {
-  options.styleGuide.verbose = true;
-  return kss(options.styleGuide);
 });
 
 // #########################
@@ -201,7 +154,7 @@ gulp.task('lint:sass-with-fail', function() {
 // ##############################
 // Watch for changes and rebuild.
 // ##############################
-gulp.task('watch', ['browser-sync', 'watch:lint-and-styleguide', 'watch:js']);
+gulp.task('watch', ['browser-sync', 'watch:lint', 'watch:js']);
 
 gulp.task('browser-sync', ['watch:css'], function() {
   if (!options.drupalURL) {
@@ -217,11 +170,10 @@ gulp.task('watch:css', ['styles'], function() {
   return gulp.watch(options.theme.sass + '**/*.scss', options.gulpWatchOptions, ['styles']);
 });
 
-gulp.task('watch:lint-and-styleguide', ['styleguide', 'lint:sass'], function() {
+gulp.task('watch:lint', ['lint:sass'], function() {
   return gulp.watch([
       options.theme.sass + '**/*.scss',
-      options.theme.sass + '**/*.twig'
-    ], options.gulpWatchOptions, ['styleguide', 'lint:sass']);
+    ], options.gulpWatchOptions, ['lint:sass']);
 });
 
 gulp.task('watch:js', ['lint:js'], function() {
@@ -231,17 +183,7 @@ gulp.task('watch:js', ['lint:js'], function() {
 // ######################
 // Clean all directories.
 // ######################
-gulp.task('clean', ['clean:css', 'clean:styleguide']);
-
-// Clean style guide files.
-gulp.task('clean:styleguide', function() {
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  return del([
-      options.styleGuide.destination + '*.html',
-      options.styleGuide.destination + 'public',
-      options.theme.css + '**/*.twig'
-    ], {force: true});
-});
+gulp.task('clean', ['clean:css']);
 
 // Clean CSS files.
 gulp.task('clean:css', function() {
