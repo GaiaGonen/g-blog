dfd834229f9cbced5c5f8ad5f07980b825876137
GG <gaiagonen@gmail.com>
2018-04-11 17:33:26 +0300

    Fixed bug with invisible elements
    The class invisible elements didn't make elements invisible,
    Fixed it with a style add to .element-invisible in the main scss file.

--- www/themes/bartik/templates/maintenance-page.tpl.php
+++ www/themes/bartik/templates/maintenance-page.tpl.php
@@ -22,9 +22,6 @@
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
 
   <div id="page-wrapper"><div id="page">
 
